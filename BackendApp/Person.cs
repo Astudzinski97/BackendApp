dd9fb0e225d939a88e1617458b9dc64da09e705e
Aleksander Studziński <astudzinski97@gmail.com>
2021-02-11 12:58:29 +0100

    Second commit

---
+++ BackendApp/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BackendApp
+{
+    public class Person
+    {
+        public virtual long Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual DateTime DateOfBirth { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, FirstName: {1}, LastName: {2}, DateOfBirth: {3}", Id, FirstName, LastName, DateOfBirth);
+
+        }
+    }
+}
---
+++ BackendApp/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace BackendApp
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "Companies",
+                routeTemplate: "company/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
