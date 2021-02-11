bbf54d77a3abc68dcb01b004cdd0944665a75f04
Aleksander Studziński <astudzinski97@gmail.com>
2021-02-11 13:12:11 +0100

    +Delete and post cations to create and delte people

---
+++ BackendApp/CreateController.cs
@@ -0,0 +1,43 @@
+﻿using HibernatingRhinos.Profiler.Appender.NHibernate;
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace BackendApp
+{
+    public class CreateController : ApiController
+    {
+        [BasicAuthentication]
+        [Route("company/create")]
+        public void Post(List<Person> people)
+        {
+            try
+            {
+                NHibernateProfiler.Initialize();
+                var mappingconfig = new Configuration();
+                mappingconfig.Configure();
+                var sefact = mappingconfig.BuildSessionFactory();
+                string query;
+                foreach (Person p in people)
+                {
+                    query = string.Format("Insert INTO Employee (FirstName, LastName, DateOfBirth, JobTitle, CompanyId) VALUES ( {0}, {1}, {2} )", p.FirstName, p.LastName, p.DateOfBirth);
+                    using (var session = sefact.OpenSession())
+                    {
+                        using (var tx = session.BeginTransaction())
+                        {
+                            IQuery q = session.CreateQuery(query);
+                            q.List();
+                            tx.Commit();
+                        }
+                    }
+                }
+            }
+            catch (MissingFieldException e)
+            {
+                Console.WriteLine("{0}", e.Message);
+            }
+        }
+    }
+}
---
+++ BackendApp/DeleteController.cs
@@ -0,0 +1,30 @@
+﻿using HibernatingRhinos.Profiler.Appender.NHibernate;
+using NHibernate;
+using NHibernate.Cfg;
+using System.Web.Http;
+
+namespace BackendApp
+{
+    public class DeleteController : ApiController
+    {
+        [BasicAuthentication]
+        [Route("company/delete/<id>")]
+        public void Delete(long id)
+        {
+            NHibernateProfiler.Initialize();
+            var mappingconfig = new Configuration();
+            mappingconfig.Configure();
+            var sefact = mappingconfig.BuildSessionFactory();
+            string query = string.Format("DELETE Person WHERE ID={0}", id);
+            using (var session = sefact.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    IQuery q = session.CreateQuery(query);
+                    q.List();
+                    tx.Commit();
+                }
+            }
+        }
+    }
+}
